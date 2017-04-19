8b263a71d66cd66a21bbb747a7040377f1228cac
Brendon <btucker1992@gmail.com>
2017-04-19 09:41:04 +0800

    passed all tests

--- Algorithms/divisibleSumPairs.py
+++ Algorithms/divisibleSumPairs.py
@@ -9,5 +9,14 @@ Created on Wed Apr 19 09:12:32 2017
 from itertools import combinations
 
 a = [1, 3, 2, 6, 1, 2]
+k = 3
 
+
+sumCount = 0
 combos = list(combinations(a, 2))
+
+
+for x in combos:
+    if sum(x[0:2]) % k == 0:
+        #for testing purposes print(x)
+        sumCount += 1
