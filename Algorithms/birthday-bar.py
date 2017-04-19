2a16d66d3fc10c39444b44a14485a1ba79029ea8
Brendon <btucker1992@gmail.com>
2017-04-19 08:55:16 +0800

    passed first test locally

---
+++ Algorithms/birthday-bar.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 19 08:37:49 2017
+
+@author: brendontucker
+"""
+
+n = 5
+s = [1, 2, 1, 3, 2]
+d = 3
+m = 2
+start = 0
+end = len(s)
+countIterater = start + m
+sumCounter = 0
+
+
+
+while countIterater <= end + 1:
+    if sum(s[start:countIterater]) == d:
+        sumCounter += 1
+        start += 1
+        countIterater += 1
+    else:
+        start += 1
+        countIterater += 1
