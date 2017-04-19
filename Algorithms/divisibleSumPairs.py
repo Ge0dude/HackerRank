34cc41ca0e79c6d4b730eb2db7d6d84d62a45b07
Brendon <btucker1992@gmail.com>
2017-04-19 09:25:19 +0800

    okay, itertools can be imported, this is nice

---
+++ Algorithms/divisibleSumPairs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 19 09:12:32 2017
+
+@author: brendontucker
+"""
+
+from itertools import combinations
+
+a = [1, 3, 2, 6, 1, 2]
+
+combos = list(combinations(a, 2))
