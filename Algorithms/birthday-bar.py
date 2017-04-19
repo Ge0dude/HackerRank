5bb04518c0a4ec74b7bd3770c15e419dcd2b80fa
Brendon <btucker1992@gmail.com>
2017-04-19 09:02:47 +0800

    changed to fucntion version, submitted and passed

--- Algorithms/birthday-bar.py
+++ Algorithms/birthday-bar.py
@@ -10,6 +10,34 @@ n = 5
 s = [1, 2, 1, 3, 2]
 d = 3
 m = 2
+   
+#function version        
+def getWays(s, d, m):
+    start = 0
+    end = len(s)
+    countIterater = start + m
+    sumCounter = 0
+    
+    
+    
+    while countIterater <= end + 1:
+        if sum(s[start:countIterater]) == d:
+            sumCounter += 1
+            start += 1
+            countIterater += 1
+        else:
+            start += 1
+            countIterater += 1
+            
+    return sumCounter
+    
+print(getWays(s, d, m))
+
+
+
+
+#local testing version
+'''
 start = 0
 end = len(s)
 countIterater = start + m
@@ -25,3 +53,4 @@ while countIterater <= end + 1:
     else:
         start += 1
         countIterater += 1
+'''     
